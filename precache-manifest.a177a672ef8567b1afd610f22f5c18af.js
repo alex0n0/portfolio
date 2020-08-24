c0987ec66eb605e1b28466d2748440823b41ebab
alex0n0 <42526086+alex0n0@users.noreply.github.com>
2020-08-24 15:35:10 +1000

    Updates

---
+++ precache-manifest.a177a672ef8567b1afd610f22f5c18af.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02eb5aa5c575e51512c06360f1d3d082",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e4bbc06d138aa4cca8c8",
+    "url": "/portfolio/static/css/main.2287763a.chunk.css"
+  },
+  {
+    "revision": "390d514d19ec6d44799d",
+    "url": "/portfolio/static/js/2.c589e8bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.c589e8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bbc06d138aa4cca8c8",
+    "url": "/portfolio/static/js/main.7ebcea58.chunk.js"
+  },
+  {
+    "revision": "acccc3776b81226aa008",
+    "url": "/portfolio/static/js/runtime-main.0f728d89.js"
+  },
+  {
+    "revision": "e224f5ed261735072b00bdb3bae20cbd",
+    "url": "/portfolio/static/media/angular-icon.e224f5ed.svg"
+  },
+  {
+    "revision": "4f548f56d2385ccc6c10ee24a7d13c47",
+    "url": "/portfolio/static/media/aspdotnetcore.4f548f56.png"
+  },
+  {
+    "revision": "89d343f5efe6c351fcd6c00871221bb0",
+    "url": "/portfolio/static/media/bitbucket.89d343f5.svg"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/portfolio/static/media/bootstrap.71e42b5a.svg"
+  },
+  {
+    "revision": "bdaccca278378c33d51a37613e6acd09",
+    "url": "/portfolio/static/media/c-sharp.bdaccca2.svg"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/portfolio/static/media/css3.d3ad67fa.svg"
+  },
+  {
+    "revision": "530516030e26a09ac38473bd5d8eff0e",
+    "url": "/portfolio/static/media/express.53051603.svg"
+  },
+  {
+    "revision": "410bbc6d0e79e62602763ab9e3a53303",
+    "url": "/portfolio/static/media/figma.410bbc6d.svg"
+  },
+  {
+    "revision": "08ec57224f5d2a9929ead8a51b22e6f8",
+    "url": "/portfolio/static/media/git.08ec5722.png"
+  },
+  {
+    "revision": "87e3ba2226e0218199da35e5a7f5457b",
+    "url": "/portfolio/static/media/hero-pattern.87e3ba22.png"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/portfolio/static/media/html5.24845c5c.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/portfolio/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "a4bd5ae0cc4cc72b48b059a29275e6e9",
+    "url": "/portfolio/static/media/jquery.a4bd5ae0.png"
+  },
+  {
+    "revision": "ba99aecd075dddfb01b64b37724165fb",
+    "url": "/portfolio/static/media/material-ui.ba99aecd.svg"
+  },
+  {
+    "revision": "fe397f2a174dad6c5f2c36cec0c1ae5e",
+    "url": "/portfolio/static/media/materializecss.fe397f2a.svg"
+  },
+  {
+    "revision": "6b0e00b3cef1bf98d3c8d90396c8580f",
+    "url": "/portfolio/static/media/mocha.6b0e00b3.svg"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/portfolio/static/media/mongodb.2aa7f7b5.svg"
+  },
+  {
+    "revision": "aa5295048f15762934de3da7c6c92c8a",
+    "url": "/portfolio/static/media/mssql.aa529504.png"
+  },
+  {
+    "revision": "dd2a5a358ea682bed45bf98bc842b3ed",
+    "url": "/portfolio/static/media/mysql.dd2a5a35.svg"
+  },
+  {
+    "revision": "94cafb0d1b4a2875215db17964f0938a",
+    "url": "/portfolio/static/media/nodejs-icon.94cafb0d.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/portfolio/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "470548b4a95cae966d0750d6720e4082",
+    "url": "/portfolio/static/media/sass.470548b4.svg"
+  },
+  {
+    "revision": "6b730f9e770ceaf47bf2c10cfb66c078",
+    "url": "/portfolio/static/media/screenshot_automender_desktop.6b730f9e.png"
+  },
+  {
+    "revision": "ee8370559370f62dc79fa22f78b503ba",
+    "url": "/portfolio/static/media/screenshot_automender_mobile.ee837055.png"
+  },
+  {
+    "revision": "aefcd662fa6c3631d4c6be8fde50e81c",
+    "url": "/portfolio/static/media/screenshot_bamazon_desktop.aefcd662.png"
+  },
+  {
+    "revision": "2a41ce7a03a7cc52ecb949f113c538ad",
+    "url": "/portfolio/static/media/screenshot_eatdaburger_desktop.2a41ce7a.png"
+  },
+  {
+    "revision": "42b71d747111cdcfc05496e3c66bbee6",
+    "url": "/portfolio/static/media/screenshot_flyin_desktop.42b71d74.png"
+  },
+  {
+    "revision": "da01b6bc447d58da8e9b66ea3d28fb16",
+    "url": "/portfolio/static/media/screenshot_flyin_mobile.da01b6bc.png"
+  },
+  {
+    "revision": "f1d4fb1c084a53354975ce71396cd455",
+    "url": "/portfolio/static/media/screenshot_liri_bot_desktop.f1d4fb1c.png"
+  },
+  {
+    "revision": "8fcb507c930ec6ab4983515a9ea3d204",
+    "url": "/portfolio/static/media/screenshot_orderaway_desktop.8fcb507c.png"
+  },
+  {
+    "revision": "af10b7023bb14e7c408529e2ab09dedf",
+    "url": "/portfolio/static/media/screenshot_orderaway_mobile.af10b702.png"
+  },
+  {
+    "revision": "33a296602710b65cefe248d09aca4e03",
+    "url": "/portfolio/static/media/screenshot_reactclickergame_desktop.33a29660.png"
+  },
+  {
+    "revision": "2015284dbac80fd0fe3c52a6933cbb4c",
+    "url": "/portfolio/static/media/screenshot_reactclickergame_mobile.2015284d.png"
+  },
+  {
+    "revision": "d9208d84bdae3d5a0655cca617ce7c35",
+    "url": "/portfolio/static/media/screenshot_star_wars_rpg_game_desktop.d9208d84.png"
+  },
+  {
+    "revision": "58a07475874b3b24a03f64673779f6db",
+    "url": "/portfolio/static/media/screenshot_star_wars_rpg_game_mobile.58a07475.png"
+  },
+  {
+    "revision": "3a5ac947595ad89f9f263ddd6597d212",
+    "url": "/portfolio/static/media/screenshot_theia_desktop.3a5ac947.png"
+  },
+  {
+    "revision": "9e8bfce4de1e3508a98fb887c593c11b",
+    "url": "/portfolio/static/media/typescript-icon.9e8bfce4.svg"
+  },
+  {
+    "revision": "23d1fdd50793a0f55a7c86018a5c2362",
+    "url": "/portfolio/static/media/unity.23d1fdd5.svg"
+  }
+]);
